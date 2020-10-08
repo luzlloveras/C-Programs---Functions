f951cafd2b50266621d0f8fe6076d6e4d7280f29
luzlloveras <luzlloveras7@gmail.com>
2020-10-08 16:50:26 -0300

    Update anio_actual.c

--- anio_actual.c
+++ anio_actual.c
@@ -3,7 +3,7 @@ int anio_actual();
 
 int main() {
     int anio = anio_actual();
-    printf("\n El anio actual es: %d", anio);
+    printf("El anio actual es: %d\n", anio);
 }
 
 int anio_actual() {
