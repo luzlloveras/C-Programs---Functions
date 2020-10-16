3d416d7b03b8676859a6944540a4a30f3a1e7117
luzlloveras <luzlloveras7@gmail.com>
2020-10-15 22:17:31 -0300

    Update 6.c

--- 6.c
+++ 6.c
@@ -5,9 +5,10 @@ primos que hay en los rangos [1000-2000], [2000,3000] y [3000,4000]. */
 int es_primo(int);
 int primos_en_rango(int, int);
 
-int main() {
+int main() 
+{
     int rango1, rango2, rango3;
-
+    
     rango1 = primos_en_rango(1000, 3000);
     rango2 = primos_en_rango(2000, 3000);
     rango3 = primos_en_rango(3000, 4000);
@@ -17,18 +18,28 @@ int main() {
 }
 
 // si el numero es primo retorna 1, caso contrario retona 0
-int es_primo(int numero) {
+int es_primo(int numero) 
+{
     int i;
-    for(i=2; i<numero; i++) {
-        if(numero % i == 0) return 0;
+    for (i = 2; i < numero; i++) 
+    {
+        if (numero % i == 0)
+        {    
+            return 0;
+        }
     }
     return 1;
 }
 
-int primos_en_rango(int num1, int num2) {
+int primos_en_rango(int num1, int num2) 
+{
     int i, contador=0;
-    for(i=num1; i<=num2; i++) {
-        if(es_primo(i)==1) contador++;
+    for (i = num1; i <= num2; i++) 
+    {
+        if (es_primo(i) == 1)
+        {
+            contador++;
+        }
     }
     return contador;
 }
