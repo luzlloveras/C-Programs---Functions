08651e76ac06a61750b5173503a60caa7b803b57
luzlloveras <luzlloveras7@gmail.com>
2020-10-15 22:08:03 -0300

    Update 1.c

--- 1.c
+++ 1.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
-int main () {
+int main () 
+{
     int a, i;
     a = 0;
-    for (i=1;i<4;i++) {
+    for (i = 1; i < 4; i++) 
+    {
         a++;
         printf("a del bloque es %d\n", a);
     }
