de75e0888b3b632465cf0f8bd7ff10dc699533b9
luzlloveras <luzlloveras7@gmail.com>
2020-10-15 22:26:12 -0300

    Update promedio.c

--- promedio.c
+++ promedio.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 float promedio(float, float);
 
-int main() {
+int main() 
+{
     float num1, num2, resultado;
     scanf("%f %f", &num1, &num2);
     resultado = promedio(num1, num2);
     printf("El resultado vale: %f\n", resultado);
 }
 
-float promedio(float x, float y) {
+float promedio(float x, float y) 
+{
     float aux = (x + y) / 2;
     return aux;
 }
