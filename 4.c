c0ffcab2f609a90c112568bc8fc30b72173ff721
luzlloveras <luzlloveras7@gmail.com>
2020-10-15 22:12:18 -0300

    Update 4.c

--- 4.c
+++ 4.c
@@ -5,27 +5,44 @@ Si es primo retorna 1, caso contrario retorna 0. */
 int es_divisible(int, int);
 int es_primo(int);
 
-int main() {
+int main() 
+{
     int ingreso;
     printf("Ingrese numero:\n");
     scanf("%d", &ingreso);
-    if(es_primo(ingreso)== 1){
+    if (es_primo(ingreso) == 1)
+    {
         printf("%d es primo\n", ingreso);
     }
-    else printf("%d no es primo\n", ingreso);
+    else 
+    {  
+        printf("%d no es primo\n", ingreso);
+    }
 }
 
 // si los numeros son divisibles retorna 1, caso contrario retona 0
-int es_divisble(int numero, int divisor) {
-    if(numero % divisor == 0) return 1;
-    else return 0;
+int es_divisble(int numero, int divisor) 
+{
+    if (numero % divisor == 0) 
+    {    
+        return 1;
+    }
+    else 
+    {    
+        return 0;
+    }
 }
 
 // si el numero es primo retorna 1, caso contrario retona 0
-int es_primo(int numero) {
+int es_primo(int numero) 
+{
     int i;
-    for(i=2; i<numero; i++) {
-        if(es_divisble(numero, i)==1) return 0;
+    for (i = 2; i < numero; i++) 
+    {
+        if (es_divisble(numero, i) == 1) 
+        {    
+            return 0;
+        }
     }
     return 1;
 }
