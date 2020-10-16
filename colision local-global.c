3566aadf109d3bb8607440a436d236b52f303da5
luzlloveras <luzlloveras7@gmail.com>
2020-10-15 22:24:01 -0300

    Update colision local-global.c

--- colision local-global.c
+++ colision local-global.c
@@ -2,13 +2,15 @@
 void funcion();
 int x = 30;
 
-int main() {
+int main() 
+{
     x++;
     funcion();
     printf("x en main vale %d\n", x);
 }
 
-void funcion () {
+void funcion () 
+{
     int x = 20;
     printf("x en funcion vale %d\n", x);
 }
