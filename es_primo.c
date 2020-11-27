0558ab9426b057f3641a314e30b12f984976f01a
luzlloveras <luzlloveras7@gmail.com>
2020-11-27 17:21:04 -0300

    Update es_primo.c

--- es_primo.c
+++ es_primo.c
@@ -10,26 +10,18 @@ int main()
     scanf("%d", &ingreso);
 
     if (es_primo(ingreso) == 1)
-    {    
         printf("%d es primo\n", ingreso);
-    }    
     else 
-    {    
         printf("%d no es primo\n", ingreso);
-    }
 }
 
 // retorna 1 si numero es divisble por divisor, caso contrario retorna 0
 int es_divisible(int numero, int divisor) 
 {
-    if (numero % divisor == 0)
-    {    
+    if (numero % divisor == 0) 
         return 1;
-    }
     else 
-    {
         return 0;
-    }
 }
 
 // retorna 1 si numero es primo, caso contrario retorna 0
@@ -37,11 +29,7 @@ int es_primo(int numero)
 {
     int i;
     for (i = 2; i < numero; i++) 
-    {
         if (es_divisible(numero, i) == 1) 
-        {    
             return 0;
-        }
-    }
     return 1;
 }
